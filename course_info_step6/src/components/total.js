9f5056442e15b1dc96d73cacf2a5e6933be89f8a
anastasiasafargalieva <94184868+anastasiasafargalieva@users.noreply.github.com>
2022-02-22 12:36:29 +0200

    2.3*: Course information step8 (reduce method))

--- course_info_step6/src/components/total.js
+++ course_info_step6/src/components/total.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises
-    return(<p> Total number of exercises  {sum} </p>)}
+    const sum = course.parts.reduce( (s,p)=> s + p.exercises, 0) 
+    //const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises
+    return(<p> <b>Total number of exercises  {sum} </b></p>)}
 
 export default Total
