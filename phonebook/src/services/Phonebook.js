a45852e56a88a4f5f2a5342fcdb684fb039fe6ab
anastasiasafargalieva <94184868+anastasiasafargalieva@users.noreply.github.com>
2022-02-25 10:23:33 +0200

    3.10-3.11

--- phonebook/src/services/Phonebook.js
+++ phonebook/src/services/Phonebook.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://shielded-basin-69229.herokuapp.com/api/persons"
+const baseUrl = "/api/persons"
 
 const addEntry = newEntry => {
     return axios.post(baseUrl, newEntry).then( response => response.data )
